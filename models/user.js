5f2b232653e06c0eaff304892732dea1597a1bd0
Ajit <ajitkr5501@gmail.com>
2025-06-29 23:49:00 +0530

    modified:   models/user.js new file:   public/images/coffee.png new file:   public/images/header.png new file:   public/uploads/1751216966173-aks.jpg modified:   views/blog.ejs modified:   views/partials/nav.ejs

--- models/user.js
+++ models/user.js
@@ -23,7 +23,7 @@ const userSchema = new Schema(
     },
     profileImageURL:{
         type:String,
-        default:'/images/default.jpg',
+        default:"/images/default.png",
     },
     role:{
         type:String,
